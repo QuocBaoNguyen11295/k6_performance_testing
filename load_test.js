494f16116a88f6d484dce544797c0b332e04b550
QuocBaoNguyen11295 <nqb11295@gmail.com>
2023-07-01 18:28:03 +0700

    Add html reporter

--- load_test.js
+++ load_test.js
@@ -1,12 +1,13 @@
 import {getUser,createUser,deleteUser,updateUser} from './helper/common_functions.js'
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 // Common things
 
 // Test setup
 export let options = {
     stages: [
-        { duration: '5m', target: 10 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-        { duration: '10m', target: 10 }, // stay at 100 users for 10 minutes.
-        { duration: '5m', target: 0 }, // ramp-down to 0 users.
+        { duration: '2m', target: 10 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '5m', target: 10 }, // stay at 100 users for 10 minutes.
+        { duration: '6m', target: 0 }, // ramp-down to 0 users.
     ]
 };
 
@@ -20,4 +21,10 @@ export default function () {
     updateUser()
     // Delete user
     deleteUser()
+}
+
+export function handleSummary(data) {
+    return {
+      "summary.html": htmlReport(data),
+    };
 }
