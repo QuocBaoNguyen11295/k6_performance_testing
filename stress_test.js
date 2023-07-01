4a1ab583d859da5dc1e12c80be65ab3748d2c402
QuocBaoNguyen11295 <nqb11295@gmail.com>
2023-07-01 19:06:56 +0700

    Add stress test

--- stress_test.js
+++ stress_test.js
@@ -5,9 +5,15 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 // Test setup
 export let options = {
     stages: [
-        { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-        { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes.
-        { duration: '5m', target: 0 }, // ramp-down to 0 users.
+        { duration: '2m', target: 100 }, // below normal load
+        { duration: '5m', target: 100 },
+        { duration: '2m', target: 200 }, // normal load
+        { duration: '5m', target: 200 },
+        { duration: '2m', target: 300 }, // around the breaking point
+        { duration: '5m', target: 300 },
+        { duration: '2m', target: 400 }, // beyond the breaking point
+        { duration: '5m', target: 400 },
+        { duration: '10m', target: 0 }, // scale down. Recovery stage.
     ]
 };
 
