f8f779fe07d002091f11108973d427cfc8acd06e
QuocBaoNguyen11295 <nqb11295@gmail.com>
2023-07-01 17:34:42 +0700

    update VU

--- load_test.js
+++ load_test.js
@@ -4,8 +4,8 @@ import {getUser,createUser,deleteUser,updateUser} from './helper/common_function
 // Test setup
 export let options = {
     stages: [
-        { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-        { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes.
+        { duration: '5m', target: 10 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '10m', target: 10 }, // stay at 100 users for 10 minutes.
         { duration: '5m', target: 0 }, // ramp-down to 0 users.
     ]
 };
